b45166a413e3b722293c94aaa316d9f4a4c73002
Mike <miguel.suarezf05@gmail.com>
2023-08-14 22:15:30 -0500

    Created dashboard folder and files

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -19,6 +19,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -40,7 +41,7 @@ export const StoreModal = () => {
     try {
       setLoading(true);
       const response = await axios.post('/api/stores', values);
-      toast.success('Store Created.');
+      window.location.assign(`/${response.data.id}`);
     } catch (error) {
       toast.error('Something went wrong.');
       console.log(error);
@@ -82,6 +83,7 @@ export const StoreModal = () => {
                   disabled={loading}
                   variant="outline"
                   onClick={storeModal.onClose}
+                  type="button"
                 >
                   Cancel
                 </Button>
